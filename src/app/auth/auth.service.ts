d5c729da88234bd6d952d13883be6fa10c6bd37d
AtharLaique <37653074+AtharLaique@users.noreply.github.com>
2020-02-19 07:21:49 +0500

    User Managment STEP 0 [ AuthService @Injectabble]

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,7 @@
 //This service is Responcible for user signup and signin and token managment
+import {Injectable} from '@angular/core';
+
+Injectable ({providedIn: 'root'});
 export class AuthService{
     constructor(){}
     signup(){
