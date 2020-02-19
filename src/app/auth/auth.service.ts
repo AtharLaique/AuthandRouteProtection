a91bcac8eaae1892ee066930ca8e1aa481148435
AtharLaique <37653074+AtharLaique@users.noreply.github.com>
2020-02-19 07:25:45 +0500

    User Managment STEP 2 [ AuthService configuring Http Service]

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,11 @@
 //This service is Responcible for user signup and signin and token managment
+import { HttpClient } from '@angular/common/http';
 import {Injectable} from '@angular/core';
 
 Injectable ({providedIn: 'root'});
 export class AuthService{
-    constructor(){}
+    constructor( private http:HttpClient){}
     signup(){
-
+    
     }
 }
