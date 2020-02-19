d11e87f99da20fb610ab165f9268580c767df78e
AtharLaique <37653074+AtharLaique@users.noreply.github.com>
2020-02-19 08:17:48 +0500

    User Managment STEP 5 [ AuthService Configuring Signup form and validations]

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
 export class AuthComponent {
+
   isLoginMode=true;
   onSwitchMode(){
     this.isLoginMode=!this.isLoginMode;
+  }
+  onLogin(user:NgForm)
+  {
+    if(!user.valid)
+    {
+      return;   
+    }
+    console.log(user.value)
+    const email=user.value.email;
+    const password=user.value.password;
+    user.reset()
+    
   }
 }
