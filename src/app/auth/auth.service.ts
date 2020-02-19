b5342fcbe05302ada43ac88b8ce8b8714f08b1b1
AtharLaique <37653074+AtharLaique@users.noreply.github.com>
2020-02-19 07:39:32 +0500

    User Managment STEP 4 [ AuthService Type casting of Responce data]

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,11 +2,19 @@
 import { HttpClient } from '@angular/common/http';
 import {Injectable} from '@angular/core';
 
+interface AuthResponceData{
+    idToken:string,
+    email:string,
+    refreshToken:string,
+    expiresIn:string,
+    localId:string
+}
+
 Injectable ({providedIn: 'root'});
 export class AuthService{
     constructor( private http:HttpClient){}
     signup(email:string , password:string ){
-       this.http.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAbg21E7AxSymXOccbQtnNjoVdIwYw9gnw',{
+        return this.http.post<AuthResponceData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAbg21E7AxSymXOccbQtnNjoVdIwYw9gnw',{
         email:email,
         password: password,
         returnSecureToken:true
